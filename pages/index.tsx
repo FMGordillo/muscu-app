ae72223bf85405d1feeeec97bc005288d8ea9b10
Facundo Martin Gordillo <me@facundogordillo.com>
2022-10-25 23:36:47 +0200

    feat: added Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import Layout from "./components/Layout";
+
 export default function Home() {
   return (
-    <div className="bg-white dark:bg-dark">
-      <h1 className="text-3xl">Hello world</h1>
-      <p>This is the future.</p>
-    </div>
+    <Layout>
+      <div className="bg-white dark:bg-dark">
+        <h1 className="text-3xl">Hello world</h1>
+        <p>This is the future.</p>
+      </div>
+    </Layout>
   );
 }
