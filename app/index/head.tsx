5400d85795b27919193aefcea00a6a02dad8fce6
Facundo Martin Gordillo <me@facundogordillo.com>
2022-10-26 10:03:58 +0200

    feat: changed structure

---
+++ app/index/head.tsx
@@ -0,0 +1,9 @@
+import { APP_NAME } from "../../../utils/constants";
+
+export default function Head() {
+  return (
+    <>
+      <title>{`${APP_NAME}`}</title>
+    </>
+  );
+}
---
+++ app/index/page.tsx
@@ -0,0 +1,10 @@
+import Layout from "./components/Layout";
+
+export default function Home() {
+  return (
+    <div className="bg-white dark:bg-dark">
+      <h1 className="text-3xl">Hello world</h1>
+      <p>This is the future.</p>
+    </div>
+  );
+}
